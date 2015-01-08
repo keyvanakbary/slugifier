8fcf1a5bd7e38292fbde10d0ee4e63c1ac7a287b
Keyvan Akbary <kiwwito@gmail.com>
2015-01-08 13:22:31 +0100

    Improve char map

--- tests/SlugifierTest.php
+++ tests/SlugifierTest.php
@@ -11,13 +11,52 @@ class SlugifierTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @test
-     * @dataProvider texts
+     * @dataProvider supportedStrings
      */
     public function shouldCreateSlug($text, $expectedSlug)
     {
         $slug = $this->slugifier->slugify($text);
 
         $this->assertEquals($expectedSlug, $slug);
+    }
+
+    public function supportedStrings()
+    {
+        return array(
+            array('Word', 'word'),
+            array('JúST å fëw wørds', 'just-a-few-words'),
+            array('J\'étudie le français', 'j-etudie-le-francais'),
+            array('An awesome slug', 'an-awesome-slug'),
+            array('  should trim  this   text ', 'should-trim-this-text'),
+            array('Práctica de acentuación', 'practica-de-acentuacion'),
+            array('Cumpleaños del muerciélago', 'cumpleanos-del-muercielago'),
+            array('هذا هو الاختبار', 'hth-ho-l-khtb-r'),
+            array('Блоґ їжачка', 'blog-jizhachka'),
+            array('Это тест', 'eto-test'),
+            array('Це тест', 'ce-test'),
+            array('Đây là một thử nghiệm', 'day-la-mot-thu-nghiem'),
+            array('Αυτή είναι μια δοκιμή', 'ayte-einai-mia-dokime'),
+            array('°¹²³@¶', '0123atp'),
+        );
+    }
+
+    /**
+     * @test
+     * @dataProvider notSupportedStrings
+     */
+    public function shouldCreateEmptySlug($text)
+    {
+        $this->assertEmpty($this->slugifier->slugify($text));
+    }
+
+    public function notSupportedStrings()
+    {
+        return array(
+            array(' ..`-. '),
+            array('테스트'),
+            array('- --'),
+            array('這是一個測試')
+        );
     }
 
     /**
@@ -28,35 +67,5 @@ class SlugifierTest extends \PHPUnit_Framework_TestCase
         $slug = $this->slugifier->slugify('Wikipedia style', '_');
 
         $this->assertEquals('wikipedia_style', $slug);
-    }
-
-    public function texts()
-    {
-        return array(
-            array('Word', 'word'),
-            array('An awesome slug', 'an-awesome-slug'),
-            array('  should trim this text ', 'should-trim-this-text'),
-            array('Práctica de acentuación', 'practica-de-acentuacion'),
-            array('Cumpleaños del muerciélago', 'cumpleanos-del-muercielago')
-        );
-    }
-
-    /**
-     * @test
-     * @dataProvider emptySlugs
-     */
-    public function shouldCreateEmptySlug($text)
-    {
-        $this->assertEmpty($this->slugifier->slugify($text));
-    }
-
-    public function emptySlugs()
-    {
-        return array(
-            array(' ..`-. '),
-            array('테스트'),
-            array('اختبار'),
-            array('- --'),
-        );
     }
 }
